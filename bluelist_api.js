f59dad7339a1bfad59e5ddcb0d241d2469108fc9
Annhilati <antbe.artis@web.de>
2025-04-24 14:46:42 +0200

    Create bluelist_api.js

---
+++ bluelist_api.js
@@ -0,0 +1,21 @@
+// Code from Annhilati
+
+function explorer_lookup(search) {
+
+    Endpoint = "localhost:3000/api/dev/search/"
+    const url = Endpoint + search
+
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            return data
+        })
+        .catch(error => {
+            console.error("Error:", error)
+        })
+}
