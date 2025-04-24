09ae1a78ac62e15fbd0f4f552ec888b63b213713
controllMaster21 <79084639+controllMaster21@users.noreply.github.com>
2025-04-24 16:15:01 +0200

    test

--- bluelist_api.js
+++ bluelist_api.js
@@ -13,9 +13,8 @@ function explorer_search(search) {
             "Content-Type": "application/json"
         }
     })
-        .then(response => {
-            console.log(response.json())}
-        )
+        .then(response => response.json())
+        .then(result => {return result})
         .catch(error => {
             console.error("Error:", error)
         });
