33366a3519f8b069f3439ed09c203a0ca368219e
controllMaster21 <79084639+controllMaster21@users.noreply.github.com>
2025-04-24 14:52:29 +0200

    added search query formating

--- bluelist_api.js
+++ bluelist_api.js
@@ -3,7 +3,7 @@
 function explorer_lookup(search) {
 
     Endpoint = "localhost:3000/api/dev/search/"
-    const url = Endpoint + search
+    const url = Endpoint + "?query=" + search.split(" ").join("+")
 
     fetch(url, {
         method: "GET",
